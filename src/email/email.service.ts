9f53524f4638a5ae96bb21301d08b28ea95755b5
Joe Monkila <jnkiwa25@gmail.com>
2024-09-03 16:17:38 +0100

    email controller

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -2,6 +2,7 @@ import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { User } from '../entity/user.entity';
 import { ConfigService } from '@nestjs/config';
+import { Email } from 'src/entity/email.entity';
 
 @Injectable()
 export class EmailService {
@@ -24,22 +25,22 @@ export class EmailService {
         });
     }
 
-    async sendMail(data:any) {
+    async sendMail(res:Email) {
 
         await this.mailerService.sendMail({
-            to: data.to,
-            from: data.from,
-            subject: data.subject,
-            text: data.content
+            to: res.to,
+            from: res.from,
+            subject: res.subject,
+            text: res.content
         });
     }
 
-    async sendMailMultiple(data:any) {
+    async sendMailMultiple(res:Email) {
 
-        for (var i = 1; i <= data.to.length; i++) {
+        for (var i = 1; i <= res.to.length; i++) {
             await this.mailerService.sendMail({
-                to: data.to[i],
-                from: data.from,
+                to: res.to[i],
+                from: res.from,
                 subject: data.subject,
                 text: data.content
             });
