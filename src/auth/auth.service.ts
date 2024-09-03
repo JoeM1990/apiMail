f4829376659ea2da2572ec5ce22edeabaf311a24
Joe Monkila <jnkiwa25@gmail.com>
2024-09-03 11:57:57 +0100

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { EmailService } from './../email/email.service';
+import { User } from './../user/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private emailService: EmailService) {}
+
+  async signUp(user: User) {
+    const token = Math.floor(1000 + Math.random() * 9000).toString();
+    // create user in db
+    // ...
+    // send welcome mail
+    await this.emailService.sendUserWelcome(user, token);
+  }
+}
