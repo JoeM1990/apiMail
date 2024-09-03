3dc4c35f16a0ce87a3bee9d6a0ef4490b61a4bbc
Joe Monkila <jnkiwa25@gmail.com>
2024-09-03 11:56:40 +0100

    auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { EmailModule } from './email/email.module';
 
-@Module({})
+@Module({
+  imports: [EmailModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
 export class AuthModule {}
