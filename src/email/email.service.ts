411fc2b54321b073500c10edc2b5ac3973788440
Joe Monkila <jnkiwa25@gmail.com>
2024-09-03 11:47:23 +0100

    email service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -9,16 +9,17 @@ export class EmailService {
   constructor(private mailerService: MailerService) {}
 
   async sendUserWelcome(user: User, token: string, content: string, subject: string) {
-    const confirmation_url = `example.com/auth/confirm?token=${token}`;
+   
+    // const confirmation_url = `example.com/auth/confirm?token=${token}`;
 
     await this.mailerService.sendMail({
       to: user.email,
       // from: '"Support Team" <support@example.com>',
-      subject: 'Welcome to Nice App! Confirm your Email',
+      subject: subject,
       template: './welcome', 
       context: { 
         name: user.name,
-        confirmation_url,
+        // confirmation_url,
       },
     });
   }
