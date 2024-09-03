3b154256c57619f3adad365e622a79bf4d2e5d8f
Joe Monkila <jnkiwa25@gmail.com>
2024-09-03 11:46:53 +0100

    email service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -8,15 +8,15 @@ import { User } from './../user/user.entity';
 export class EmailService {
   constructor(private mailerService: MailerService) {}
 
-  async sendUserWelcome(user: User, token: string) {
+  async sendUserWelcome(user: User, token: string, content: string, subject: string) {
     const confirmation_url = `example.com/auth/confirm?token=${token}`;
 
     await this.mailerService.sendMail({
       to: user.email,
-      // from: '"Support Team" <support@example.com>', // override default from
+      // from: '"Support Team" <support@example.com>',
       subject: 'Welcome to Nice App! Confirm your Email',
-      template: './welcome', // `.ejs` extension is appended automatically
-      context: { // filling <%= %> brackets with content
+      template: './welcome', 
+      context: { 
         name: user.name,
         confirmation_url,
       },
