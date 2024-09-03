f534472c685919b2ec51de9cf45193c5150c0d97
Joe Monkila <jnkiwa25@gmail.com>
2024-09-03 11:12:19 +0100

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // no need to import into other modules
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
