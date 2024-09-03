e16d66d93f5636aed9b269072eca8650b79e37ef
Joe Monkila <jnkiwa25@gmail.com>
2024-09-03 11:44:42 +0100

    email module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // no need to import into other modules
     }),
+    EmailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
