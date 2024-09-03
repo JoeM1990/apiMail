55a42c4ba80b436140dcae407faf297547e79cff
Joe Monkila <jnkiwa25@gmail.com>
2024-09-03 11:45:22 +0100

    email module

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,4 +1,39 @@
-import { Module } from '@nestjs/common';
 
-@Module({})
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { Global, Module } from '@nestjs/common';
+import { MailService } from './email.service';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+
+@Global()
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('MAIL_HOST'),
+          secure: false,
+          auth: {
+            user: config.get('SMTP_USERNAME'),
+            pass: config.get('SMTP_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"Nice App" <${config.get('SMTP_USERNAME')}>`,
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new EjsAdapter(),
+          options: {
+            strict: false,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [EmailService],
+  exports: [EmailService],
+})
 export class EmailModule {}
