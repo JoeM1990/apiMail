00b02b08175c7f44a25302717295c18f46248043
Joe Monkila <jnkiwa25@gmail.com>
2024-09-04 08:04:33 +0100

    delete entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
