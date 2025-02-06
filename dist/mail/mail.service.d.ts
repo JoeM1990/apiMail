798101a57ef9e9763a8bbd6ca1057d6023677976
Joe Monkila <jnkiwa25@gmail.com>
2025-02-06 13:49:46 +0100

    update service

--- dist/mail/mail.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class MailService {
-    private config;
-    private transporter;
-    constructor(config: ConfigService);
-    sendMail(to: string, fromName: string, subject: string, text: string): Promise<any>;
-    sendMailMultiple(to: string[], fromName: string, subject: string, text: string): Promise<any>;
-}
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Mail Api';
+    return 'Mail Api By MonkilaTech';
   }
 }
