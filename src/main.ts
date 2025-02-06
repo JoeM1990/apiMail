6dadc7389c981d9fcdf21eb674a736ab641bd709
Joe Monkila <jnkiwa25@gmail.com>
2025-02-06 14:54:37 +0100

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://monkila-tech.com', 'http://localhost:4200'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    allowedHeaders: 'Content-Type, Authorization', 
+    credentials: true, 
+  });
+
   await app.listen(3000);
 }
 bootstrap();
